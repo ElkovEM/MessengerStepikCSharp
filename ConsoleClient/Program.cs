661e0fcb4251788ae268b794c496e2ea86654430
ElkovEM <elkov.egor@gmail.com>
2022-04-23 01:39:14 +0300

    Добавлен класс Message

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace MessengerStepikCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Message msg = new Message();
+            Console.WriteLine("Начало!");
+            Console.WriteLine(msg.ToString());
+        }
+    }
+}
